843d4c70bd1bbb378de5d305298542de9697d0d2
Ali Adnan Julfar <ali.1522@dubai-sms.com>
2023-06-24 14:50:11 +0400

    Initial Commit

---
+++ unit04/unit04App.swift
@@ -0,0 +1,17 @@
+//
+//  unit04App.swift
+//  unit04
+//
+//  Created by Ali Julfar on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct unit04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
